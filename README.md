13d70013c8c59a857bfc70b10b9808996a85ddfa
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-31 19:23:53 +0800

    added files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# tf-lambda-311224
-Reference to Lesson 2.13 In-Class-Activity
+### Use Terraform to set up an API to perform Create, Read, Update, Delete (CRUD) operations against items in a DynamoDB table. (Details are in Lesson 2.13 In-Class-Activity labsheet)
+
+<p>AWS Services used: Lambda, API Gateway and DynamoDB.
+
+<p>Step 1: Create DynamoDB table
+<br>Step 2: Create Lambda Function
---
+++ table.tf
@@ -0,0 +1,31 @@
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+    name           = "yap-topmovies"
+    billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "year"
+    #range_key      = "Genre"
+
+  attribute {
+    name = "year"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  /* global_secondary_index {
+    name               = "TitleIndex"
+    hash_key           = "Title"
+    range_key          = "Author"
+    #write_capacity     = 10
+    #read_capacity      = 10
+    projection_type    = "ALL"
+    #non_key_attributes = ["UserId"]
+  } */
+
+  tags = {
+    Name        = "yap-topmovies"
+    Environment = "test"
+  }
+}
